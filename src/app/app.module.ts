e37afccd4f126a7ff5b401421d9259bc7819ee87
carolroc <carolroc02@gmail.com>
2022-03-04 21:04:43 -0300

    2 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule } from '@angular/router';
+import { TabuadaComponent } from './tabuada/tabuada.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent }, //passa os parametros caminho e o componente
+      { path: 'sobre', component: SobreComponent },
+      { path: 'tabuada', component: TabuadaComponent }
+    ])//configura as rotas a partir da raiz
+  ],
+  declarations: [ AppComponent, HelloComponent, TabuadaComponent, SobreComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/tabuada/tabuada.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabuada',
+  templateUrl: './tabuada.component.html',
+  styleUrls: ['./tabuada.component.css'],
+})
+export class TabuadaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+  tabuadaCinco() {
+    let tabuada = [];
+
+    for (var i = 0; i < 10; i++) {
+      tabuada.push(5 * i);
+    }
+
+    return tabuada;
+  }
+}
